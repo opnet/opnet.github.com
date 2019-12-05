d861c6c9975d2a6f79c2e7aba7d6f5829c50b10c
Joel Klassen <joel.klassen@ek3.com>
2019-12-05 08:56:18 -0500

    Add JSDocs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Row, Col, Image, Alert } from 'react-bootstrap'
 
-import useLazyList, { onScrollEnd } from './lazylist'
+import debounce from 'lodash.debounce'
+import useLazyList from './lazylist'
 
+/**
+ * Notify the provided callback whenever the scroll status is close to the bottom of the document
+ *
+ * @param {Function} onUpdate function to notify
+ * @returns {Function} function to remove listener
+ */
+export function onVerticalScrollEnd (onUpdate = () => {}) {
+  // TODO: Make offset configurable
+  const minOffset = 100
+
+  const listener = debounce(() => {
+    const scrollOffset = document.documentElement.offsetHeight - (window.innerHeight + document.documentElement.scrollTop)
+    if (minOffset >= scrollOffset) {
+      // the bottom of the page has been reached, fire an update event
+      onUpdate()
+    }
+  }, 100)
+
+  window.addEventListener('scroll', listener)
+  return () => window.removeEventListener('scroll', listener)
+}
+
+/**
+ * Renders a Columns of images within a single row.
+ *
+ * @param {Object} props react props
+ * @param {Array<String>} props.assets set of image assets to render
+ * @returns {React.Element} A JSX rendering of images within Cols within a single Row
+ */
 export function ImageRow ({ assets = [] } = {}) {
   return (
     <Row noGutters={true}>
@@ -15,14 +45,26 @@ export function ImageRow ({ assets = [] } = {}) {
   )
 }
 
+/**
+ * Renders a collection of image assets within a Container. The list of assets
+ * provided will only initially be a few assets. As the user scrolls to the bottom
+ * of the provided assets more assets will be lazily loaded.
+ *
+ * @param {Object} props react props
+ * @param {Array<String>} props.assets set of assets to render
+ * @returns {React.Element} A JSX rendering of a Container containing Rows of Images
+ */
 export function AssetContainer ({ assets = [] } = {}) {
+  // TODO: If the body is smaller than the window the scroll listener won't fire.
+  // Automatigically fill enough of the viewport with images such that it can scroll
   const [ list, more ] = useLazyList({
     list: assets,
-    initialLength: 3 * 5, // do math to find required screen size ... or 5 rows
+    initialLength: 3 * 9, // some cool algorithm to detect how many rows will fill the viewport ... or just 9 rows
     jumpSize: 3 * 3, // 3 rows of assets
-    onUpdate: onScrollEnd
+    onUpdate: onVerticalScrollEnd
   })
 
+  // build rows with three sets of images each
   const imageRows = []
   while (list.length) {
     imageRows.push(<ImageRow assets={list.splice(0, 3)} />)
@@ -32,14 +74,21 @@ export function AssetContainer ({ assets = [] } = {}) {
       <Container fluid={true}>
         {imageRows}
       </Container>
-      <Alert variant={more ? 'primary' : 'secondary'}>
-        <Alert.Heading>{more ? 'There\'s so much more left to see!' : 'Aww that\'s it'}</Alert.Heading>
-      </Alert>
+      {more ? null :
+        <Alert variant='secondary'>
+          <Alert.Heading>That's it!</Alert.Heading>
+        </Alert>
+      }
     </>
   )
 }
 
-export default function App (props) {
+/**
+ * Renders the asset container using the list of fetched assets from the manifest.
+ *
+ * @returns {AssetContainer} A JSX rendering of the AssetContainer component
+ */
+export default function App () {
   const [assets, setAssets] = useState([])
   useEffect(() => {
     (async () => {
--- src/lazylist.js
+++ src/lazylist.js
@@ -1,18 +1,15 @@
 import { useEffect, useState } from 'react'
 
-import debounce from 'lodash.debounce'
-
-export function onScrollEnd (onUpdate = () => {}) {
-  const listener = debounce(() => {
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-      // the bottom of the page has been reached, fire an update event
-      onUpdate()
-    }
-  }, 100)
-  window.addEventListener('scroll', listener)
-  return () => window.removeEventListener('scroll', listener)
-}
-
+/**
+ * A hook that will return a subset of the provided list, providing more of the list when the update callback fires.
+ *
+ * @param {Object} props react props
+ * @param {Array<T>} props.list collection of items to lazily return
+ * @param {Integer} [props.initialLength=0] length of the firs subset of assets returned
+ * @param {Integer} [props.jumpsize=1] length of subsequent subsets to return
+ * @param {Function} props.onUpdate notify function to tell hook to return more items
+ * @returns {[ Array<T>, Boolean ]} subset of assets and a boolean, true if more items are available in the list
+ */
 export default function useLazyList ({ list = [], initialLength = 0, jumpSize = 1, onUpdate = () => {} } = {}) {
   const [itemCount, setItemCount] = useState(initialLength)
 
